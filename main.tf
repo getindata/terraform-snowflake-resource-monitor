744cbdd2ab4df0055dc93c726c29c78c99ceecfc
Dominik Gniewek-Węgrzyn <dominik.gniewek@getindata.com>
2024-07-31 10:16:09 +0200

    fix: custom role enabled parameter logic

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ module "snowflake_custom_role" {
 
   name            = each.key
   attributes      = ["RMN", one(snowflake_resource_monitor.this[*].name)]
-  enabled         = local.create_default_roles && lookup(each.value, "enabled", true)
+  enabled         = lookup(each.value, "enabled", true)
   descriptor_name = lookup(each.value, "descriptor_name", "snowflake-role")
 
   role_ownership_grant = lookup(each.value, "role_ownership_grant", "SYSADMIN")
